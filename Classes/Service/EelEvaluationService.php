7d45012c1e811a4d3b0c0f7284f179ca682de7c9
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2023-05-22 22:51:47 +0200

    TASK: Error handling: Dont print out eel expression again (if ParserException)

--- Classes/Service/EelEvaluationService.php
+++ Classes/Service/EelEvaluationService.php
@@ -2,6 +2,7 @@
 
 namespace Flowpack\NodeTemplates\Service;
 
+use Neos\Eel\ParserException;
 use Neos\Flow\Annotations as Flow;
 use Neos\Eel\Utility as EelUtility;
 use Neos\Eel\CompilingEvaluator;
@@ -44,8 +45,10 @@ class EelEvaluationService
         $contextVariables = array_merge($this->defaultContextVariables, $contextVariables);
         try {
             return EelUtility::evaluateEelExpression($expression, $this->eelEvaluator, $contextVariables);
+        } catch (ParserException $parserException) {
+            throw new \RuntimeException('EEL Expression in NodeType template could not be parsed.', 1684788574212, $parserException);
         } catch (\Exception $exception) {
-            throw new \Exception(sprintf('EEL Expression "%s" in NodeType template caused an error.', $expression), 1684761760723, $exception);
+            throw new \RuntimeException(sprintf('EEL Expression "%s" in NodeType template caused an error.', $expression), 1684761760723, $exception);
         }
     }
 }
