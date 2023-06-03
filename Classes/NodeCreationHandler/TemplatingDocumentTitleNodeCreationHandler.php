f845b9197893b8bdeb4c179822717d33d1bd7bad
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2023-06-03 14:07:29 +0200

    WIP: TASK: Explain stuff

--- Classes/NodeCreationHandler/TemplatingDocumentTitleNodeCreationHandler.php
+++ Classes/NodeCreationHandler/TemplatingDocumentTitleNodeCreationHandler.php
@@ -8,6 +8,13 @@ use Neos\Flow\Annotations as Flow;
 use Neos\Neos\Ui\NodeCreationHandler\DocumentTitleNodeCreationHandler;
 use Neos\Neos\Ui\NodeCreationHandler\NodeCreationHandlerInterface;
 
+/**
+ * Augments the original neos ui document title node creation handler, which takes care of setting the "uriPathSegment" based of the title.
+ * This handler steps in when a node has a node template with the property `uriPathSegment`.
+ * In this case we will prevent the original handler from being called, as we handle setting the `uriPathSegment` ourselves and the original handler will just override our `uriPathSegment` again.
+ *
+ * @todo once we have sorting with https://github.com/neos/neos-ui/pull/3511 we can put our handler at the end instead.
+ */
 class TemplatingDocumentTitleNodeCreationHandler implements NodeCreationHandlerInterface
 {
     /**
